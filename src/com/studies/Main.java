f4e70d3c402906139db876afc81bb0b7b39d4e80
gleao00 <gleao@minsait.com>
2021-07-06 14:35:51 +0200

    1 commit

---
+++ src/com/studies/Main.java
@@ -0,0 +1,10 @@
+package com.studies;
+
+public class Main {
+
+    static int number = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Test Number: " + number);
+    }
+}
