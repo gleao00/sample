6cbea468eafae1b6a1708ffefb351908ff58f1e4
gleao00 <gleao@minsait.com>
2021-07-06 14:58:23 +0200

    1 commit

--- src/com/studies/Main.java
+++ src/com/studies/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     static int number = 1;
 
+    static private Animal a = new Animal("Gabriel", 10);
+
     public static void main(String[] args) {
         System.out.println("Test Number: " + number);
+        System.out.println("Animal Name: " + a.getName() + "\n" + "Animal Age: " + a.getAge());
     }
 }
